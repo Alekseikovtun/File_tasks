896b0f1bd12261a537aeb98637429be0a580facf
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2022-09-25 00:09:41 +0300

    Created defs for reading file & for counting words

---
+++ main.py
@@ -0,0 +1,39 @@
+import json
+import xml.etree.ElementTree as ET
+from pprint import pprint
+
+def read_files(name):
+    with open(name, 'r', encoding = 'utf-8') as f:
+        data = json.load(f)
+        original_text = '' 
+        for items in data['rss']['channel']['items']:
+            original_text += ' ' + items['description']
+        return original_text
+
+def count_word(original_text):
+    a = 0
+    to_list = original_text.split(' ')
+    word_value ={}
+    for word in to_list:
+        if len(word) > 6:
+            a += 1
+            word_value[word]=a
+        else:
+            word_value[word]=1
+    return word_value
+
+
+
+
+
+print()
+# while True:
+    # name = input("Enter file name: newsafr.json or newsafr.xml or enter exit: ")
+name = "newsafr.json"
+    # if name == "newsafr.json":
+top_10 = count_word(read_files(name))
+print(top_10)
+    # elif name == "exit":
+    #     break
+    # else:
+    #     print('Incorrect input, try again')
